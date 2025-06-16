1c067996e3120acfd89345e7eda76ca77ab45fbb
jonathanvra <jonathan.vra@gmail.com>
2025-06-16 15:40:04 -0500

    feat(myApp.js): agregar middleware para servir archivos estáticos en /public

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ let app = express();
 
 absolutePath = __dirname + '/views/index.html';
 
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.sendFile(absolutePath);
 });
