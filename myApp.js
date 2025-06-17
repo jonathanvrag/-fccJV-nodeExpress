55e09e04606066b1dd3a1eaf13980eb37ac34c73
jonathanvra <jonathan.vra@gmail.com>
2025-06-17 09:49:04 -0500

    feat(myApp.js): agregar ruta GET para /name que devuelve el nombre completo en formato JSON

--- myApp.js
+++ myApp.js
@@ -39,4 +39,12 @@ app.get('/:word/echo', (req, res) => {
   res.json({ echo: word });
 });
 
+app.get('/name', (req, res) => {
+  const firstName = req.query.first;
+  const lastName = req.query.last;
+
+  res.json({ name: `${firstName} ${lastName}` });
+
+})
+
 module.exports = app;
