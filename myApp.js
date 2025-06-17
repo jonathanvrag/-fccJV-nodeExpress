7095297677bfefce1a80a051ad52761bfd9f1aab
jonathanvra <jonathan.vra@gmail.com>
2025-06-17 09:11:49 -0500

    fix(myApp.js): corregir cierre de función y estandarizar comillas en el mensaje JSON

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ absolutePath = __dirname + '/views/index.html';
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
-})
+});
 
 app.use('/public', express.static(__dirname + '/public'));
 
@@ -16,11 +16,22 @@ app.get('/', (req, res) => {
 });
 
 app.get('/json', (req, res) => {
-  let message = "Hello json";
+  let message = 'Hello json';
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     message = message.toUpperCase();
   }
   res.json({ message });
 });
+
+app.get(
+  '/now',
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
 
 module.exports = app;
